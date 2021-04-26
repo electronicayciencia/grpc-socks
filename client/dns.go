7affb5594363c3f44a67c6b5c25bf54e732fc1da
Electronica y Ciencia <electronicayciencia@gmail.com>
2021-04-26 14:44:59 +0200

    Use local DNS resolver

--- client/dns.go
+++ client/dns.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"net"
+	"log"
 
 	"github.com/coocood/freecache"
 
@@ -21,16 +22,30 @@ var nameCtxKey = struct{}{}
 func (d DNSResolver) Resolve(ctx context.Context, name string) (context.Context, net.IP, error) {
 	ctx = context.WithValue(ctx, nameCtxKey, name)
 
+	log.Println("[DNS] resolve:", name)
+
 	if v, err := d.cache.Get([]byte(name)); err == nil {
+		log.Println("[DNS]", name, "cached")
 		return ctx, v, nil
+	}
+
+	addr, err := net.ResolveIPAddr("ip", name)
+	if err == nil {
+		log.Println("[DNS]", name, "resolved using local:", addr.IP)
+		d.cache.Set([]byte(name), addr.IP, expireSeconds)
+		return ctx, addr.IP, err
 	}
 
 	ipResp, err := proxyClient.ResolveIP(ctx, &pb.IPAddr{
 		Address: name,
 	})
+
 	if err == nil {
+		log.Println("[DNS]", name, "resolved using remote:", ipResp.Data)
 		d.cache.Set([]byte(name), ipResp.Data, expireSeconds)
+		return ctx, ipResp.Data, err
 	}
 
-	return ctx, ipResp.Data, err
+	log.Println("[DNS] Unable to resolve", name)
+	return ctx, nil, err
 }
